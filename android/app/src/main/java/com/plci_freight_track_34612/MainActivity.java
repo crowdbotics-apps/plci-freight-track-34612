7e42b77d82fdbe4511ee1967b78f74627a446205
Crowdbotics <team@crowdbotics.com>
2022-04-05 20:56:24 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/plci_freight_track_34612/MainActivity.java
@@ -0,0 +1,15 @@
+package com.plci_freight_track_34612;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "plci_freight_track_34612";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "plci_freight_track_34612",
+  url: "https://plci_freight_track_34612.botics.co",
+  api: "https://plci_freight_track_34612.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
